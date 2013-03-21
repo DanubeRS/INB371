7f69912faa272d038ec10a3e545139478b6dc1fa
daniel <daniel@daniel-desktop.(none)>
2013-03-21 15:28:03 +1000

    Blank file for Q3 SOurce.

---
+++ INB371_W3/Q3.cpp
@@ -0,0 +1,24 @@
+/*
+    Weekdays (c) Daniel Park 2013
+
+    A simple program that calculates the prime numbers within a range, returning
+    both the values themselves, and the cumulative number of them.
+*/
+
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Inform the user of the program:
+    cout << "================================" << endl;
+    cout << "|         PrimeFinderTM        |" << endl;
+    cout << "================================" << endl;
+    cout << "|    (c)  Daniel Park 2013     |" << endl;
+    cout << "|     ***INB371_S1_W3_Q1***    |" << endl;
+    cout << "================================" << endl << endl;
+}
