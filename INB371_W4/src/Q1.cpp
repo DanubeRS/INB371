7939a99fa549fcfdc748688f908ed62a25d2b9b0
DanubeRS <Danubers@gmail.com>
2013-03-24 15:50:25 +1000

    [ISS-1] Fixed randomisation

--- INB371_W4/src/Q1.cpp
+++ INB371_W4/src/Q1.cpp
@@ -6,6 +6,12 @@
     v0.1
     ====
     -Initial creation
+    v1.0
+    ====
+    -Partial solution commited
+    v1.1
+    ====
+    -Attempted fix for Issue #1 (Randomize not working)
 
 
 */
@@ -19,6 +25,7 @@ using namespace std;
 
 //Function Prototypes
 
+double SeedAndRand(double min, double max);		//Used to circumvent issue 1
 
 //Program Entry Point
 int main()
@@ -41,9 +48,14 @@ int main()
    	double trials[runs];	//Build an array with space for every run
    	double upper = 1.0; double lower = 0.0;
 
+   	Randomize(); // Randomise to only be called ONCE at start of program
+
    	for (int i = 0; i < runs; i++){
+		/*	===FAILED TO WORK (See issue #1)
 		Randomize();	//Reseed
 		trials[i] = RandomReal(lower, upper);	//Implicitly cast to ensure compatability
+		*/
+		trials[i] = RandomReal(upper, lower);
 		cout << "Trial " << i << " gave " << trials[i] << endl;
    	}
 
@@ -54,4 +66,13 @@ int main()
    	}
    	cout << "Mean of all results: " << runningTotal / runs << endl;
 
+}
+
+/*
+	Conglomorate function used in an attempt to solve Issue #1
+	Accepts upper and lower limits for a randomisation, and will return the random value.
+	Reseeds the randomisation pool before calling tool function.
+*/
+double SeedAndRand(double min, double max){
+	return RandomReal(min, max);
 }
