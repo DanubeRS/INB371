987526c3f3295eb229e26173abe80acb9c7c97c9
daniel <daniel@daniel-TM2.(none)>
2013-03-25 11:36:34 +1000

    Generate file for W4_Q2

---
+++ INB371_W4/src/Q2.cpp
@@ -0,0 +1,37 @@
+/*
+    Heads and Tails (c) Daniel Park 2013
+    
+    A program which uses the interfacing features of c++ to generate a heads-or-tails
+    probability simulation.
+
+    v0.1
+    ====
+    -Initial creation
+
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include "supplied/random.h"
+
+using namespace std;
+
+//Function Prototypes
+
+double SeedAndRand(double min, double max);		//Used to circumvent issue 1
+
+//Program Entry Point
+int main()
+{
+
+    //Inform the user of the program:
+    cout << "================================" << endl;
+    cout << "|        Heads or Tails        |" << endl;
+    cout << "================================" << endl;
+    cout << "|    (c)  Daniel Park 2013     |" << endl;
+    cout << "|     ***INB371_S1_W3_Q5***    |" << endl;
+    cout << "================================" << endl << endl;
+
+    
+}
