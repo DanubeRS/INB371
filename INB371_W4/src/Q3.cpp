b8fdaa453d967805f2fb9dce66a8b38daafe32b7
daniel <daniel@daniel-TM2.(none)>
2013-03-25 14:57:53 +1000

    Create program template, and update info comment header

---
+++ INB371_W4/src/Q3.cpp
@@ -0,0 +1,30 @@
+/*
+    CaseCracker (c) Daniel Park 2013
+
+    A program which uses a string library to change cases
+
+    v0.1
+    ====
+    -Initial creation
+
+*/
+
+//===Library Declerations
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+//===Function Prototypes
+
+//Entry Point
+int main()
+{
+
+    //Inform the user of the program:
+    cout << "================================" << endl;
+    cout << "|        Heads or Tails        |" << endl;
+    cout << "================================" << endl;
+    cout << "|    (c)  Daniel Park 2013     |" << endl;
+    cout << "|     ***INB371_S1_W3_Q5***    |" << endl;
+    cout << "================================" << endl << endl;
+}
