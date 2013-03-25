988e3080f0b9b4f934778708a12a0b3f26115ff5
daniel <daniel@daniel-TM2.(none)>
2013-03-25 14:45:37 +1000

    Completed program. Bumped to v1.0.

--- INB371_W4/src/Q2.cpp
+++ INB371_W4/src/Q2.cpp
@@ -1,12 +1,17 @@
 /*
     Heads and Tails (c) Daniel Park 2013
-    
+
     A program which uses the interfacing features of c++ to generate a heads-or-tails
     probability simulation.
 
     v0.1
     ====
     -Initial creation
+
+    v1.0
+    ====
+    -Completed aplliaction
+    -Added verbosity prompt
 
 */
 
@@ -19,7 +24,9 @@ using namespace std;
 
 //Function Prototypes
 
-double SeedAndRand(double min, double max);		//Used to circumvent issue 1
+string VerboseResult(bool res); //Used to convert a boolean to a string representation
+void DeviationAnalysis(int th, int tr); //Checking for individual result verbosity
+bool CheckForIndividualVerbosity();
 
 //Program Entry Point
 int main()
@@ -33,5 +40,86 @@ int main()
     cout << "|     ***INB371_S1_W3_Q5***    |" << endl;
     cout << "================================" << endl << endl;
 
-    
+    int trials = 0; int totalHeads = 0; //FOr tracking of trialResult
+    bool verbIndividual;  //Verbose individual results
+    const double CHANCE = 0.5;  //AVOID MAGIC NUMBERS!
+
+    cout << "How many trials?:" << endl;
+    cin >> trials;
+    cout << "Verbose individual results? (y/n):" << endl;
+    verbIndividual = CheckForIndividualVerbosity();
+
+    bool trialResults[trials];  //Store in array for possible finer result analysis
+
+    Randomize();    //Reseed the randomisation engine
+    for (int i = 0; i < trials; i++)
+    {
+        trialResults[i] = RandomChance(CHANCE);
+    }
+
+    //Count the amount of heads (and as such, reverse calculate tails)
+    for (int i = 0; i < trials; i++)
+    {
+        if (trialResults[i])
+        {
+            totalHeads++;
+        }
+    }
+
+    //Print Results
+    if (verbIndividual)
+    {
+        for (int i = 0; i < trials; i++)
+        {
+            cout << "Toss " << i << " gave " << VerboseResult(trialResults[i]) << endl;
+        }
+    }
+    cout << "Totals:" << endl << "\t Heads: " << totalHeads << "\t Tails :" << trials - totalHeads << endl;
+    DeviationAnalysis(totalHeads, trials);
+    return 0;
+
+}
+/*
+  A function which takes a boolean value, and returns the name of a coin face.
+*/
+string VerboseResult(bool res)
+{
+    if (res)
+    {
+        return "Head";
+    }
+    else if (!res)
+    {
+        return "Tail";
+    }
+    else
+    {
+        return "";    //Add an exception possibly?
+    }
+}
+void DeviationAnalysis(int th, int tr)
+{
+    double probabilityCalc = (double) th / (double) tr;
+    double fiftyFifty = 0.5;
+    cout << "Heads gave tested probability of " << probabilityCalc << endl;
+    cout << "This was a " << fiftyFifty - probabilityCalc << " deviation from 50\%" << endl;
+}
+bool CheckForIndividualVerbosity()
+{
+    string ans = "";
+    cout << "Prompt>>";
+    cin >> ans;
+    if (ans == "y")
+    {
+        return true;
+    }
+    else if (ans == "n")
+    {
+        return false;
+    }
+    else
+    {
+        cout << "Invalid response. Please try again." << endl;
+        CheckForIndividualVerbosity();
+    } //Cycle function
 }
