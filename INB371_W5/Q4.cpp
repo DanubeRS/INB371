ddc874984539e3caa3901c4640fe8cb1d557da73
daniel <daniel@daniel-desktop.(none)>
2013-03-29 22:38:44 +1000

    Initially create Q4 source

---
+++ INB371_W5/Q4.cpp
@@ -0,0 +1,24 @@
+/*
+    Heads and Tails (c) Daniel Park 2013
+
+    A program which uses the OOP features of c++ to rewrite the random interface as used in W4 tasks.
+    v0.1
+    ====
+    -Initial creation
+
+*/
+
+//Program Entry Point
+int main()
+{
+
+    //Inform the user of the program:
+    cout << "================================" << endl;
+    cout << "|       Random Class Demo      |" << endl;
+    cout << "================================" << endl;
+    cout << "|    (c)  Daniel Park 2013     |" << endl;
+    cout << "|     ***INB371_S1_W5_Q3***    |" << endl;
+    cout << "================================" << endl << endl;
+
+    return 0;
+}
