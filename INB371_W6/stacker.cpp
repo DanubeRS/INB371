92fcdd1d4f0c9900fa2bf61c2fa93e4ed6f7dcf4
Daniel Park <DanubeRS@gmail.com>
2013-06-01 01:01:07 +1000

    modified stacker.cpp to include a new type.

--- INB371_W6/stacker.cpp
+++ INB371_W6/stacker.cpp
@@ -5,30 +5,49 @@
 
 #include <iostream>
 #include "stack.h"
+#include "random.h"
 
 using namespace std;
 
-int main() {
+int main()
+{
 
     Stack<int> intStack;
     Stack<string> stringStack;
+    Stack<float> floatStack;
 
     string words[] = {"a", "an", "pan", "pant", "paint", "taping", "plating", "pleating" };
 
-    for (int i = 1; i < 10; i++) {
+    for (int i = 1; i < 10; i++)
+    {
         intStack.push(i);
     }
 
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++)
+    {
         stringStack.push(words[i]);
     }
 
-    while (!intStack.isEmpty()) {
+    Random r;
+
+    for (int i = 0; i < 10; i++)
+    {
+        floatStack.push(r.RandomInteger(1, 100) * 1.123f);
+    }
+
+    while (!intStack.isEmpty())
+    {
         cout << intStack.pop() << endl;;
     }
 
-    while (!stringStack.isEmpty()) {
-        cout << stringStack.pop() << endl;;
+    while (!stringStack.isEmpty())
+    {
+        cout << stringStack.pop() << endl;
+    }
+
+    while (!floatStack.isEmpty())
+    {
+        cout << floatStack.pop() << endl;
     }
 
     return 0;
